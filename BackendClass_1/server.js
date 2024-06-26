6fa2155124eec00ce9c3a8e43ddc55792e00abc7
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-26 19:44:10 +0530

    server instantiated

---
+++ BackendClass_1/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+// server instantiated | To run:  node server.js
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
