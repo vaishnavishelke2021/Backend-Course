badc0b497dafe28fe1dc7a79d8d45ea25e59b6b1
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-26 21:47:28 +0530

    POST request created

--- BackendClass_1/server.js
+++ BackendClass_1/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+
 // server instantiated | To run:  node server.js
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
@@ -9,4 +10,12 @@ app.listen(3000, () => {
 // creating route
 app.get("/", (req, res) => {
   res.send("Welcome to my server");
+});
+
+// creating post request to post/submit data
+app.post("/api/cars", (req, res) => {
+  const { name, brand } = req.body;
+  console.log(name);
+  console.log(brand);
+  res.send("Car submitted successfully");
 });
