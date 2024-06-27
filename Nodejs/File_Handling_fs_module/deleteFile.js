dd9bfaec78fbc86fdbc07c50fd0f8b67fcaa25dc
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-27 21:30:50 +0530

    get statistics of file

--- Nodejs/File_Handling_fs_module/deleteFile.js
+++ Nodejs/File_Handling_fs_module/deleteFile.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
 
 // parameters : (filename to delete)
-fs.unlinkSync("./test2.txt");
+// fs.unlinkSync("./test2.txt");
 
+
+// -----------------------------------------------------------
+// get statistics of file
+console.log(fs.statSync("./test.txt"));
