2c2d07a88e2591ea86e5a164db0002e6c94a4c18
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-27 23:23:15 +0530

    get url for every request : {req.url}

--- Nodejs/Building_HTTP_Server/index.js
+++ Nodejs/Building_HTTP_Server/index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 //create log for every request using fs module
 const myServer = http.createServer((req, res) => {
-  const log = `Time: ${Date.now()} => new request received\n`;
+  const log = `Time: ${Date.now()} | URL: ${req.url} => new request received\n`;
   fs.appendFile("log.txt", log, (err, data) => {
     if (err) throw err;
     res.end("Hello from server");
