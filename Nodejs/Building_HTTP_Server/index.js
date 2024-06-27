176ea7a984109056b633f1f2128fed536589eac7
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-27 23:19:49 +0530

    created log for every request

--- Nodejs/Building_HTTP_Server/index.js
+++ Nodejs/Building_HTTP_Server/index.js
@@ -1,8 +1,13 @@
 const http = require("http");
+const fs = require("fs");
 
+//create log for every request using fs module
 const myServer = http.createServer((req, res) => {
-  console.log("New request received");
-  res.end("Hello from server");
+  const log = `Time: ${Date.now()} => new request received\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    if (err) throw err;
+    res.end("Hello from server");
+  });
 });
 
 myServer.listen(8000, () => {
