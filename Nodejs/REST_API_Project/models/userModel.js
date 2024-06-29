1f95afd47dccf0a642720963d0d21b65a7e3ad90
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-06-29 22:00:08 +0530

    Schema and model added to models folder

---
+++ Nodejs/REST_API_Project/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    gender: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+//Model:
+const User = mongoose.model("User", userSchema); // Create a model using the schema
+
+module.exports = User;
