cd7949aad85b0958cdb11a6f8fa1505d82b4e58b
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-01 22:43:58 +0530

    mongoDB connected

--- Nodejs/URL_Shortener/index.js
+++ Nodejs/URL_Shortener/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
-
+const urlRoute = require("./routes/urlRoute");
+const { connectMongoDb } = require("./connection.js");
 const PORT = 3000;
+
+connectMongoDb("mongodb://localhost:27017/short-url").then(() =>
+  console.log("Mongodb connected")
+);
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
