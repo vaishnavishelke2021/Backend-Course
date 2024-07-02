e9e225de839d4403d2eb1018de26990fd091ebda
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-02 09:53:56 +0530

    route to redirect url created

--- Nodejs/URL_Shortener/index.js
+++ Nodejs/URL_Shortener/index.js
@@ -3,6 +3,7 @@ const app = express();
 const urlRoute = require("./routes/urlRoute");
 const { connectMongoDb } = require("./connection.js");
 const PORT = 3000;
+const URL = require("./models/urlModel.js");
 
 connectMongoDb("mongodb://localhost:27017/url-shortener").then(() =>
   console.log("Mongodb connected")
@@ -14,5 +15,21 @@ app.use(express.urlencoded({ extended: false }));
 
 //Route
 app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    { shortId },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+
+  res.redirect(entry.redirectUrl);
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
