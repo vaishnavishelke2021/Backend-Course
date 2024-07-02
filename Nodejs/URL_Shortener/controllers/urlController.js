631b8d304684cbd6aee93bc9ad67c5bf916e57dd
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-02 09:29:50 +0530

    syntax corrected

--- Nodejs/URL_Shortener/controllers/urlController.js
+++ Nodejs/URL_Shortener/controllers/urlController.js
@@ -1,17 +1,18 @@
-const { shortId } = require("shortid");
+const shortId = require("shortid");
 const URL = require("../models/urlModel");
 
 async function handleGenerateNewShotURL(req, res) {
   const body = req.body;
   if (!body.url) return res.status(400).json({ error: "url required" });
-  const shortID = shortId(8);
+  const shortID = shortId();
+
   await URL.create({
     shortId: shortID,
     redirectUrl: body.url,
     visitHistory: [],
   });
 
-  return res.json({ id: shortId });
+  return res.json({ id: shortID });
 }
 
 module.exports = { handleGenerateNewShotURL };
