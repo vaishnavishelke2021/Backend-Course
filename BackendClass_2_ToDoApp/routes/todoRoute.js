e60c3beac494fd928d3e308b6db447943d0f7d41
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-04 00:07:50 +0530

    UpdateTodo added to the todoRoute

--- BackendClass_2_ToDoApp/routes/todoRoute.js
+++ BackendClass_2_ToDoApp/routes/todoRoute.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { handleCreateTodo } = require("../controllers/CreateToDo");
 const { handleGetTodo } = require("../controllers/GetTodo");
 const { handleGetTodoById } = require("../controllers/GetTodoById");
+const { handleUpdatTodo } = require("../controllers/UpdateTodo");
 const router = express.Router();
 
 router.post("/createTodo", handleCreateTodo);
 router.get("/getTodo", handleGetTodo);
 router.get("/getTodo/:id", handleGetTodoById);
+router.put("/updateTodo/:id", handleUpdatTodo);
 
 module.exports = router;
