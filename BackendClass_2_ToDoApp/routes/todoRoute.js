35861f19440085b56e656e34c56fdbe8eb023ada
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-03 23:51:57 +0530

    GetTodoById added to the todoRoute

--- BackendClass_2_ToDoApp/routes/todoRoute.js
+++ BackendClass_2_ToDoApp/routes/todoRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { handleCreateTodo } = require("../controllers/CreateToDo");
 const { handleGetTodo } = require("../controllers/GetTodo");
+const { handleGetTodoById } = require("../controllers/GetTodoById");
 const router = express.Router();
 
 router.post("/createTodo", handleCreateTodo);
 router.get("/getTodo", handleGetTodo);
+router.get("/getTodo/:id", handleGetTodoById);
 
 module.exports = router;
