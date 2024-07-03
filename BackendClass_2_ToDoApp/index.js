f8899e09d5f5d045944016a0095a1878d6f715a7
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-03 21:40:35 +0530

    PORT logic added

--- BackendClass_2_ToDoApp/index.js
+++ BackendClass_2_ToDoApp/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+require("dotenv").config;
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// PORT
+const PORT = process.env.PORT || 4000;
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
