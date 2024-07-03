f8f96500413de7cd8d8dfbe14125e1eea4c06267
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-03 21:45:10 +0530

    model added (created schema)

---
+++ BackendClass_2_ToDoApp/models/ToDoModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 100,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    required: true,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+    required: true,
+  },
+});
+
+const ToDo = mongoose.model("TodO", todoSchema);
+module.exports = ToDo;
