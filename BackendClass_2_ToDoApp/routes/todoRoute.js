412d57bfa947d5cf54aec9660df408f01660502c
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-03 23:40:21 +0530

    GetTodo added to the todoRoute

--- BackendClass_2_ToDoApp/routes/todoRoute.js
+++ BackendClass_2_ToDoApp/routes/todoRoute.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { handleCreateTodo } = require("../controllers/CreateToDo");
+const { handleGetTodo } = require("../controllers/GetTodo");
 const router = express.Router();
 
 router.post("/createTodo", handleCreateTodo);
+router.get("/getTodo", handleGetTodo);
 
 module.exports = router;
