2c6fd034adea5c55d187c06adbe62f124f56fd45
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-04 00:19:07 +0530

    DeleteTodo added to the todoRoute

--- BackendClass_2_ToDoApp/routes/todoRoute.js
+++ BackendClass_2_ToDoApp/routes/todoRoute.js
@@ -3,11 +3,13 @@ const { handleCreateTodo } = require("../controllers/CreateToDo");
 const { handleGetTodo } = require("../controllers/GetTodo");
 const { handleGetTodoById } = require("../controllers/GetTodoById");
 const { handleUpdatTodo } = require("../controllers/UpdateTodo");
+const { handleDeleteTodo } = require("../controllers/DeleteTodo");
 const router = express.Router();
 
 router.post("/createTodo", handleCreateTodo);
 router.get("/getTodo", handleGetTodo);
 router.get("/getTodo/:id", handleGetTodoById);
 router.put("/updateTodo/:id", handleUpdatTodo);
+router.delete("/deleteTodo/:id", handleDeleteTodo);
 
 module.exports = router;
