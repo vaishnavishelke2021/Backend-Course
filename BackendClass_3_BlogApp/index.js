65548002020a13005e03427d7b8fd0f05dff203a
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-04 21:12:30 +0530

    default route added

--- BackendClass_3_BlogApp/index.js
+++ BackendClass_3_BlogApp/index.js
@@ -3,6 +3,11 @@ const app = express();
 
 const PORT = 3000;
 
+// default route
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
