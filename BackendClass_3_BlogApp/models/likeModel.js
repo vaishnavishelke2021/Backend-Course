dc4f4fa2b58b37650d2f5f4a11ecc17ea8c24ea9
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-04 22:55:49 +0530

    likeModel (schema) created

---
+++ BackendClass_3_BlogApp/models/likeModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("../config/connection");
+
+const likeSchema = new mongoose.Schema({
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post", //reference to the post model
+  },
+  user: {
+    type: String,
+    required: true,
+  },
+});
+
+const Like = mongoose.model("Like", likeSchema);
+module.exports = Like;
