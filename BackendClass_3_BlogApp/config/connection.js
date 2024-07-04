739a900f28c186c6ffb04b1efaba60193edbc868
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-04 21:29:19 +0530

    syntax modified

--- BackendClass_3_BlogApp/config/connection.js
+++ BackendClass_3_BlogApp/config/connection.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 
 async function connectToMongoDb() {
   try {
-    mongoose.connect("mongodb://127.0.0.1/27017/BlogAppdatabase");
+    await mongoose.connect("mongodb://127.0.0.1:27017/BlogAppdatabase");
     console.log("Connected to MongoDB");
   } catch (e) {
     console.error("Error: ", e);
     console.log("Error while connecting to MongoDB", e.message);
+    process.exit(1); // a function used to terminate the current Node.js process with an exit code of 1
   }
 }
 
