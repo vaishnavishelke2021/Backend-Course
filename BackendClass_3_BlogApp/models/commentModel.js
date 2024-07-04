0e76ec90ddae10cf6898e2ff926ad93421bf15a2
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-04 22:55:03 +0530

    commentModel (schema) created

---
+++ BackendClass_3_BlogApp/models/commentModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("../config/connection");
+
+const commentSchema = new mongoose.Schema(
+  {
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post", //reference to the post model
+    },
+    user: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+module.exports = Comment;
