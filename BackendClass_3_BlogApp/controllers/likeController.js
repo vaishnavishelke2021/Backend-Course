10c1ee5413ede7b1f0a8491b9a261d8237e85315
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-12 21:59:16 +0530

    controller added to unlike post

--- BackendClass_3_BlogApp/controllers/likeController.js
+++ BackendClass_3_BlogApp/controllers/likeController.js
@@ -14,7 +14,7 @@ async function likePost(req, res) {
 
     const updatePost = await Post.findByIdAndUpdate(
       post,
-      { $push: { likes: savedLike._id } }, //$push : to add new comment in comments array
+      { $push: { likes: savedLike._id } }, //$push : to add new like in likes array
       { new: true }
     )
       .populate("likes")
@@ -31,4 +31,28 @@ async function likePost(req, res) {
   }
 }
 
-module.exports = { likePost };
+// ----------------unlike post--------------------
+async function unlikePost(req, res) {
+  try {
+    const { post, like } = req.body;
+    //find and delete like
+    const deletedLike = await Like.findOneAndUpdate({ post: post, _id: like });
+    // update the post collection
+    const updatePost = await Post.findByIdAndUpdate(
+      post,
+      { $pull: { likes: deletedLike._id } }, //$pull : to delete like from like array
+      { new: true }
+    );
+
+    res.json({
+      post: updatePost,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+}
+
+module.exports = { likePost, unlikePost };
--- BackendClass_3_BlogApp/routes/blogRoutes.js
+++ BackendClass_3_BlogApp/routes/blogRoutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { createComment } = require("../controllers/commentController");
 const { createPost, getAllPost } = require("../controllers/postController");
-const { likePost } = require("../controllers/likeController");
+const { likePost, unlikePost } = require("../controllers/likeController");
 
 router.post("/comments/create", createComment);
 router.post("/posts/create", createPost);
 router.get("/posts", getAllPost);
 router.post("/likes/like", likePost);
+router.post("/likes/unlike", unlikePost);
 
 module.exports = router;
