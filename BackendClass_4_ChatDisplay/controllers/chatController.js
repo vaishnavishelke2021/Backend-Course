40d9f21826033bbe4771b9f30620e2e0896283a2
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-20 09:20:50 +0530

    styling added to /chats

--- BackendClass_4_ChatDisplay/controllers/chatController.js
+++ BackendClass_4_ChatDisplay/controllers/chatController.js
@@ -3,10 +3,11 @@ const Chat = require("../models/chaatModel");
 async function showChats(req, res) {
   try {
     const chats = await Chat.find();
-    res.status(200).json({
-      status: "success",
-      data: chats,
-    });
+    // res.status(200).json({
+    //   status: "success",
+    //   data: chats,
+    // });
+    res.render("index.ejs", { chats });
   } catch (e) {
     console.log(e);
     res.status(500).json({
