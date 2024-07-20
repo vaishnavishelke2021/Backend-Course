640e47b1189763a2ae53ccb88009d90d3c3a5266
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-20 07:01:27 +0530

    controller created to show all chats

---
+++ BackendClass_4_ChatDisplay/controllers/chatController.js
@@ -0,0 +1,19 @@
+const Chat = require("../models/chaatModel");
+
+async function showChats(req, res) {
+  try {
+    const chats = await Chat.find();
+    res.status(200).json({
+      status: "success",
+      data: chats,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+}
+
+module.exports = { showChats };
