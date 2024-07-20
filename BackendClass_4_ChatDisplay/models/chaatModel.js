e9ffbb4fcdc7737a7d476e86f7b94b00edb431c1
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-20 06:41:13 +0530

    chat model created

---
+++ BackendClass_4_ChatDisplay/models/chaatModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    maxLength: 50,
+  },
+  from: {
+    type: String,
+    required: true,
+  },
+  to: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Chat = mongoose.model("Chat", chatSchema);
+module.exports = Chat;
