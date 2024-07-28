7b1cf14eea07a6efdaa812d5006ae953fca724a4
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-28 20:36:25 +0530

    updated

--- UserManagementCRUD/server.js
+++ UserManagementCRUD/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
-dotenv.config({ path: "config.env" });
+dotenv.config({ path: ".env" });
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+//log requests
+app.use(morgan("tiny"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
