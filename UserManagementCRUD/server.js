760be722d02fee69cabd1a1ad0c869c131878f8c
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-28 22:01:27 +0530

    added body-parser, view engine, & assets

--- UserManagementCRUD/server.js
+++ UserManagementCRUD/server.js
@@ -2,16 +2,29 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
 
 dotenv.config({ path: ".env" });
 const PORT = process.env.PORT || 8080;
 
+//log requests
+app.use(morgan("tiny"));
+
+//body parser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+//set view engine
+app.set("view engine", "ejs");
+// app.set("views", path.resolve(__dirname));
+
+// load assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
-//log requests
-app.use(morgan("tiny"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
