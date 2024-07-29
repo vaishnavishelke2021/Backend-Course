41b621a4b77b43de26ee80f40db25e07ded12ec7
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-29 22:47:10 +0530

    updated and added actions to the form

--- UserManagementCRUD/server.js
+++ UserManagementCRUD/server.js
@@ -6,33 +6,29 @@ const bodyparser = require("body-parser");
 const path = require("path");
 const router = require("./routes/routes");
 const connectTomongoDB = require("./connection");
+const methodOverride = require("method-override");
 
 dotenv.config({ path: ".env" });
 const PORT = process.env.PORT || 8080;
 
-//log requests
+// Log requests
 app.use(morgan("tiny"));
 
-//body parser
+// Body parser
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
-//set view engine
-app.set("view engine", "ejs");
-// app.set("views", path.resolve(__dirname));
-
-// override with POST having ?_method=PUT
-const methodOverride = require("method-override");
+// Method override
 app.use(methodOverride("_method"));
 
-// load assets
-app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
-app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+// Set view engine
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
 
-//routes
-app.use("/api/users", router);
+// Routes
+app.use("/api", router);
 
-//database connection
+// Database connection
 connectTomongoDB();
 
 app.listen(PORT, () => {
