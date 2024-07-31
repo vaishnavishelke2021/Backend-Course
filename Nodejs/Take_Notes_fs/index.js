fde59feb37e3861d09f52d5a5165bf7b57c622ac
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-31 23:11:12 +0530

    get route created to get detailed note

--- Nodejs/Take_Notes_fs/index.js
+++ Nodejs/Take_Notes_fs/index.js
@@ -23,8 +23,19 @@ app.get("/", (req, res) => {
 //create route
 app.post("/create", (req, res) => {
   //   console.log(req.body);
-  fs.writeFile(`./files/${req.body.title}`, req.body.note, (err) => {
-    res.redirect("/");
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.note,
+    (err) => {
+      res.redirect("/");
+    }
+  );
+});
+
+//get note detail
+app.get("/files/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show", { filename: req.params.filename, filedata: filedata });
   });
 });
 
