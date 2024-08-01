072158531a9839b5dd1bfe5642d5c7180692ade1
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-08-01 12:45:29 +0530

    basic setup

---
+++ Nodejs/Add_Users_Mongodb/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//routes
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
